ed30f4d283e1c1155c83a4ae79fc98f73d0c7bcb
Simone Palugini <simonepalugini02@gmail.com>
2020-03-16 20:14:24 +0100

    Esercizio finito

--- Palugini_XML/MainWindow.xaml.cs
+++ Palugini_XML/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Palugini_XML
 {
@@ -23,6 +25,45 @@ namespace Palugini_XML
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        bool token = false;
+
+        private void btn_aggiungi_Click(object sender, RoutedEventArgs e)
+        {
+            token = true;
+            lst_ListaAlunni.Items.Clear();
+            Task.Factory.StartNew(()=>CaricaDati());
+        }
+        
+        private void CaricaDati()
+        {
+            Studente studenti = new Studente();
+            string path = @"ListaAlunni.xml";
+            XDocument xmlDoc = XDocument.Load(path);
+            XElement xmlstudenti = xmlDoc.Element("studenti");
+            var xmlstudente = xmlstudenti.Elements("studente");
+
+            foreach (var item in xmlstudente)
+            {
+                XElement xmlLastName = item.Element("cognome");
+                XElement xmlFirstName = item.Element("nome");
+                XElement xmlPresenze = item.Element("presenze");
+                Studente s = new Studente();
+                s.Cognome = xmlLastName.Value;
+                s.Nome = xmlFirstName.Value;
+                s.Presenze = Convert.ToInt32(xmlPresenze.Value);
+                studenti = s;
+                Dispatcher.Invoke(() => lst_ListaAlunni.Items.Add(studenti));
+                Thread.Sleep(800);
+                if (!token)
+                    break;
+            }                   
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            token = false;
         }
     }
 }
---
+++ Palugini_XML/Studente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palugini_XML
+{
+    public class Studente
+    {
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public int Presenze { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome}, presenze: {Presenze}";
+        }
+    }
+}
